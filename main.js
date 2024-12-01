8b0130e66d2f8c4268450779bcdd383ca1c60d8d
DostonbekZaripov <dastandev0110@gmail.com>
2024-12-02 01:24:45 +0500

    Footer qismi qoldi

---
+++ main.js
@@ -0,0 +1,69 @@
+// const mevalar = "apple banan apple orange apple";
+// const word = "apple";
+// const mevalar2 = mevalar.split(word).length - 1;
+// console.log(mevalar2)
+
+// var str = '#fozil#web#brain#';
+// let str2 = '#';
+
+// for(let i = 0; i<str.length; i++){
+//     if(str.length[i] == str2){
+//         console.log("Salom")
+//     }
+// }
+// let str = "467812";
+// let str2 = str.split();
+// for(let i = 0; i<str2.length; i++){
+//     if(str2.length < 9){
+//         console.log(str2.length)
+//     }
+//     if(str2.length > 0 && str2.length < 9){
+//         console.log(str2.length)
+//     }  
+// }
+
+
+// 1-masala
+// let str = 'JavascriptIsAmazing';
+// let start = 4;
+// let end = 14;
+// let result = str.substring(start,end);
+// console.log(result)
+
+
+// 2-masala
+// let str = "  Find spaces here  ".trim();
+// let space = " ";
+// let res = [  ];
+// for(let i = 0; i < str.length; i++){
+//     if(str[i] === " "){
+//         res.push(i)
+//     }
+// }
+// console.log(res)
+
+// 3-masala
+
+// let str = "apple orange apple banana orange".split(" ");
+// let str2 = []
+
+// for(let i = 0; i < str.length ; i++){
+//     if(str.length[i] == str.length[i]){
+//         str2.push(i)
+//     }
+// }
+
+// console.log(str2)
+
+
+// 4-masala
+// let str = "apple123orange456banana789";
+// let str2 = str.split("")
+// let abc = "qwertyuiopasdfghjklzxcvbnm";
+// let abc2 = abc.split("")
+// let arr = []
+// for(let i = 0; i<str2.length; i++){
+//     let newarr = str2 == abc2.includes()
+//     arr.push(newarr)
+// }
+// console.log(arr)
